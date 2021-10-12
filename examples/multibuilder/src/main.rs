3bfcbbea4305f37195e3806e4310666dc294cf51
test@test.test <test@test.test>
2021-10-12 11:38:04 +0100

    Change recursive example to one more similar to the one that caused the original issue. Takes at least 5 minutes to compile (unclear if terminates) on laptop 10310U.

--- examples/multibuilder/src/main.rs
+++ examples/multibuilder/src/main.rs
@@ -1,13 +1,14 @@
-use spirv_builder::{MetadataPrintout, SpirvBuilder};
+use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
 
 fn main() {
     let result = SpirvBuilder::new(
         concat!(env!("CARGO_MANIFEST_DIR"), "/../shaders/compute-shader"),
         "spirv-unknown-spv1.3",
     )
-    .print_metadata(MetadataPrintout::DependencyOnly)
-    .multimodule(true)
-    .build()
-    .unwrap();
+        .capability(Capability::Int8)
+        .print_metadata(MetadataPrintout::DependencyOnly)
+        .multimodule(true)
+        .build()
+        .unwrap();
     println!("{:#?}", result);
 }
--- examples/shaders/compute-shader/src/lib.rs
+++ examples/shaders/compute-shader/src/lib.rs
@@ -1,35 +1,49 @@
 #![cfg_attr(
-    target_arch = "spirv",
-    feature(register_attr),
-    register_attr(spirv),
-    no_std
+target_arch = "spirv",
+feature(register_attr),
+register_attr(spirv),
+no_std
 )]
-// HACK(eddyb) can't easily see warnings otherwise from `spirv-builder` builds.
-#![deny(warnings)]
 
 extern crate spirv_std;
 
 use glam::UVec3;
 use spirv_std::glam;
+use spirv_std::glam::{Mat3, Vec3, Vec4};
 #[cfg(not(target_arch = "spirv"))]
 use spirv_std::macros::spirv;
 
 // Adapted from the wgpu hello-compute example
 
-pub fn collatz(n: u32) -> Option<u32> {
-    if n == 1{
-        return Some(n)
+fn index_to_transform(index: usize, raw_data: &[u8]) -> Transform2D
+{
+    // TODO
+    Transform2D{
+        own_transform: Mat3::IDENTITY,
+        parent_offset: 0
     }
-    else if n % 2 == 0 {
-            collatz(n / 2)
+}
+
+const SIZE_OF_TRANSFORM: usize = core::mem::size_of::<Transform2D>();
+
+#[derive(Clone)]
+struct Transform2D {
+    own_transform: Mat3,
+    parent_offset: i32,
+}
+
+trait GivesFinalTransform {
+    fn get_final_transform(&self, raw_data: &[u8]) -> Mat3;
+}
+
+impl GivesFinalTransform for (i32, Transform2D) {
+    fn get_final_transform(&self, raw_data: &[u8]) -> Mat3 {
+        if self.1.parent_offset == 0 {
+            self.1.own_transform
+        } else {
+            let parent_index = self.0 + self.1.parent_offset;
+            self.1.own_transform.mul_mat3(&((parent_index as i32, index_to_transform(parent_index as usize, raw_data)).get_final_transform(raw_data)))
         }
-    else {
-            // Overflow? (i.e. 3*n + 1 > 0xffff_ffff)
-            if n >= 0x5555_5555 {
-                return None;
-            }
-            // TODO: Use this instead when/if checked add/mul can work: n.checked_mul(3)?.checked_add(1)?
-            collatz(3 * n + 1)
     }
 }
 
@@ -37,16 +51,10 @@ pub fn collatz(n: u32) -> Option<u32> {
 #[spirv(compute(threads(64)))]
 pub fn main_cs(
     #[spirv(global_invocation_id)] id: UVec3,
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] prime_indices: &mut [u32],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] raw_data: &mut [u8],
+    #[spirv(position)] output_position: &mut Vec4,
 ) {
     let index = id.x as usize;
-    prime_indices[index] = unwrap_or_max(collatz(prime_indices[index]));
-}
-
-// Work around https://github.com/EmbarkStudios/rust-gpu/issues/677
-fn unwrap_or_max(option: Option<u32>) -> u32 {
-    match option {
-        Some(inner) => inner,
-        None => u32::MAX,
-    }
+    let final_transform = (index as i32, index_to_transform(index, raw_data)).get_final_transform(raw_data);
+    *output_position = final_transform.mul_vec3(Vec3::new(0.1, 0.2, 0.3)).extend(0.0);
 }
